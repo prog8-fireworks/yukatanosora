a1e38c950388b095cd6d75f45088439341def266
haru <hinagoro5656@gmail.com>
2025-08-23 18:04:23 +0900

    feat: CSS変数と基本スタイルを追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import '../app.css';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>ゆかたのそら</title>
 </svelte:head>
 
 {@render children?.()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-</p>
+<!-- HTMLの中身だけ書く -->
+<h1 class="title">これがトップページ "/"でアクセスする</h1>
+<p>ゆかた編集エディタがこのページかな</p>
+<a href="/complete">完成ページ</a>
+
+<!-- スタイル(CSS) -->
+<style>
+	/* 以下は例なので消してください */
+	.title {
+		font-size: 2rem;
+		font-weight: bold;
+		color: var(--color-primary);
+	}
+</style>
