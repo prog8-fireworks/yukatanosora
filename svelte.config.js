3598f552e90c19682bf96445f85b292fdb6e2571
haru <hinagoro5656@gmail.com>
2025-08-23 18:59:45 +0900

    fix: 不要なオプションを削除し、wrangler.jsoncにaccount_idを追加

--- svelte.config.js
+++ svelte.config.js
@@ -9,18 +9,8 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			// See below for an explanation of these options
 			config: 'wrangler.jsonc',
-			platformProxy: {
-				configPath: undefined,
-				environment: undefined,
-				persist: undefined
-			},
-			fallback: 'plaintext',
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
+			fallback: 'plaintext'
 		})
 	}
 };
