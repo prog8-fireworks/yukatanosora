7f2051c04dd2d05f5e527a4dddf16202f66722c0
misaki838 <chichimeixiao13@gmail.com>
2025-08-29 11:38:55 +0900

    style: ボタンのホバー効果と背景色を変更し、全体のデザインを改善しました

--- src/routes/complete/+page.svelte
+++ src/routes/complete/+page.svelte
@@ -295,8 +295,8 @@
 	}
 
 	.btn:hover {
-		transform: translateY(-2px);
-		box-shadow: 0 6px 8px rgba(0, 0, 0, 0.15);
+		transform: translateY(2px);
+		box-shadow: 0 8px 6px rgba(0.15, 0, 0, 0);
 	}
 
 	.btn i {
@@ -304,16 +304,16 @@
 	}
 
 	.download-btn {
-		background: linear-gradient(90deg, #1f64c6, #0d3b66);
+		background: linear-gradient(90deg, #9b59b6, #e1a9be);
 	}
 
 	.share-btn {
-		background: linear-gradient(90deg, #c965c6, #9b59b6);
+		background: linear-gradient(90deg, #e1a9be, #9b59b6);
 	}
 
 	.new-create-btn {
-		background: #e6e6fa;
-		color: #353333;
+		background: #e1a9be;
+		color: #f8f6f6;
 		position: fixed;
 		bottom: 50px;
 		right: 50px;
@@ -321,7 +321,10 @@
 		padding: 15px 30px;
 		z-index: 1000; /* mi--:ブラウザを小さく開いてる時にクリックできなかったので、これを追加しました */
 	}
-
+	.new-create-btn:hover {
+		background-color: #9b59b6; /* ホバー時の色 */
+		transform: translateY(2px); /* ちょっと浮いた感じ */
+	}
 	/* 小物のスタイル */
 	.geta,
 	.higasa,
